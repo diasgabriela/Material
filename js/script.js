7bde9c8af942788db5f0afdbdf86998b76b1fb66
diasgabriela <gabi_rj05@yahoo.com.br>
2018-09-09 16:35:28 -0300

    Added Jquey

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+$(document).ready(function(){
+    $("#button").click(function(){
+        $("#description").fadeToggle();
+    });
+});
+
+$(document).ready(function(){
+
+    $("#spotify").css('display', 'none');
+    $("#btnspotify").click(function(){
+        $("#spotify").fadeToggle();
+        
+    });
+});
+
+
 M.AutoInit();
